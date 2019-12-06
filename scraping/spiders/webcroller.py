d324a66fd18d634c7939743978e5759286bad630
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2019-12-06 17:33:37 +0100

    the

--- scraping/spiders/webcroller.py
+++ scraping/spiders/webcroller.py
@@ -2,53 +2,49 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider
-from scraping.items import ScrapingItem
 
 
+class LinkItem(scrapy.Item):
+    url = scrapy.Field()
+    referer = scrapy.Field()
+    status = scrapy.Field()
+    
+    
 class Webcroller(CrawlSpider):
-    name = "webcrawl"
+    name = "webcroller"
 
-    # The domains that are allowed (links to other domains are skipped)
-    allowed_domains = ["diwanee-serbia.rs"]
+    # allowed_domains = ["diwanee-serbia.rs"]
+    # allowed_domains = ["diwanee.com"]
+    # allowed_domains = ["spottertravel.com"]
+    # allowed_domains = ["street.rs"]
+    # allowed_domains = ["www.yasmina.com"]
+    allowed_domains = ["test.street.rs"]
 
-    # The URLs to start with
-    start_urls = ['http://www.diwanee-serbia.rs']
+    # start_urls = ['http://www.diwanee-serbia.rs']
+    # start_urls = ['https://www.diwanee.com/']
+    # start_urls = ['https://www.spottertravel.com/']
+    # start_urls = ['https://www.street.rs']
+    # start_urls = ['https://www.yasmina.com']
+    start_urls = ['http://test.street.rs']
 
-    # This spider has one rule: extract all (unique and canonicalized) links, follow them and parse them using the parse_items method
-    rules = [
+    # handle_httpstatus_list = [404]
+    # handle_httpstatus_all = True
+    custom_settings = {'HTTPERROR_ALLOW_ALL': True}
+    
+    rules = (
         Rule(
-            LinkExtractor(
-                canonicalize=False,
-                unique=True
-            ),
-            follow=True,
-            callback="parse_items"
-        )
-    ]
+            LinkExtractor(), 
+            callback='parse_item', 
+            follow=True
+        ),
+    )
 
-    # Method which starts the requests by visiting all URLs specified in start_urls
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+    def parse_item(self, response):
+        # if response.status == 404:
+            link = LinkItem()
 
-    # Method for parsing items
-    def parse_items(self, response):
-        # The list of items that are found on the particular page
-        items = []
-        # Only extract canonicalized and unique links (with respect to the current page)
-        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
-        # Now go through all the found links
-        for link in links:
-            # Check whether the domain of the URL of the link is allowed; so whether it is in one of the allowed domains
-            is_allowed = False
-            for allowed_domain in self.allowed_domains:
-                if allowed_domain in link.url:
-                    is_allowed = True
-            # If it is allowed, create a new item and add it to the list of found items
-            if is_allowed:
-                item = ScrapingItem()
-                #item['url_from'] = response.url
-                item['url_to'] = link.url
-                items.append(item)
-        # Return all the found items
-        return items
+            link['url'] = response.url
+            link['status'] = response.status
+            link['referer'] = response.request.headers.get('Referer')
+
+            return link
